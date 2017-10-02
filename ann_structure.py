c27e6ab867da28c77db026686322ba799082f98a
Sergey Korkhov <korhovsergey@hotmail.com>
2017-10-02 18:47:55 -0400

    First working version.

---
+++ ann_structure.py
@@ -0,0 +1,182 @@
+'''
+Structural functions needed for the NN:
+
+init_params(layer_dims):
+	params
+
+update_parameters(parameters, grads, learning_rate):
+	parameters
+
+linear_forward(A_prev, W, b):
+	Z, cache
+
+linear_backward(dA, cache):
+	dA, dW, db
+
+activation_forward(A_prev, W, b, activation='relu'):
+	A, cache
+
+activation_backward(dA, cache, activation='relu'):
+	dA_prev, dW, db
+
+L_forward(X, parameters):
+	AL, caches
+
+L_backward(AL, Y, caches):
+	grads
+
+NOT:
+L_layer_model(X, Y, layer_dims, learning_rate=0.01, num_iterations= , print_cost= )
+	parameters
+'''
+
+from ann_utils import *
+import numpy as np
+
+def init_params(layer_dims):
+    '''
+    Initializess parameters according to He et al initialization.
+    '''
+    params = {}
+    L = len(layer_dims)
+    
+    for i in range(1, L):
+        params['W' + str(i)] = np.random.randn(layer_dims[i], layer_dims[i-1]) * np.sqrt(2 / layer_dims[i-1])
+        params['b' + str(i)] = np.zeros((layer_dims[i], 1))
+    
+    return params
+
+
+def update_parameters(parameters, grads, learning_rate):
+    
+    L = len(parameters) // 2
+    
+    for i in range(L):
+        parameters['W' + str(i + 1)] = parameters['W' + str(i + 1)] - learning_rate * grads['dW' + str(i + 1)]
+        parameters['b' + str(i + 1)] = parameters['b' + str(i + 1)] - learning_rate * grads['db' + str(i + 1)]
+    
+    return parameters
+
+
+def linear_forward(A_prev, W, b):
+    '''
+    Computes linear finction of the parameters W and b, and preveous activation A
+    
+    Inputs:
+    A_prev = activation from the previous layer, A.shape = (n_(l-1), m)
+    W = parameters of layer l, W.shape = (n_l, n_(l-1))
+    b = bias of layer l, b.shape = (n_l, 1)
+    
+    Outputs:
+    Z = computed linear function of the node, Z.shape = (n_l, m)
+    cache = variable that stores parameters A_prev, Wl, bl to be used in back propargation
+    '''
+    
+    Z = np.dot(W, A_prev) + b
+    cache = (A_prev, W, b)
+    
+    return Z, cache
+
+def linear_backward(dZ, cache):
+    
+    A_prev, W, b = cache
+    
+    dW = np.dot(dZ, A_prev.T)
+    db = np.sum(dZ, axis=1, keepdims=True)
+    dA_prev = np.dot(W.T, dZ)
+    
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+    
+    return dA_prev, dW, db
+
+def activation_forward(A_prev, W, b, activation='relu'):
+    
+    Z, lin_cache = linear_forward(A_prev, W, b)
+    
+    if activation == 'relu':
+        A, activ_cache = relu(Z)
+    
+    if activation == 'sigmoid':
+        A, activ_cache = sigmoid(Z)
+    
+    if activation == 'softmax':
+        A, activ_cache = softmax(Z)
+    
+    
+    cache = lin_cache, activ_cache # cache = ((A_prev, W, b), Z)
+    
+    return A, cache
+
+def activation_backward(dA, cache, activation):
+    
+    lin_cache, activ_cache = cache # lin_cache = (A, W, b), activ_cache = Z
+    
+    if activation == 'relu':
+        dZ = relu_backward(dA, activ_cache)
+    
+    if activation == 'sigmoid':
+        dZ = sigmoid_backward(dA, activ_cache)
+    
+    if activation == 'softmax':
+        dZ = softmax_backward(dA, activ_cache)
+    
+    dA_prev, dW, db = linear_backward(dZ, lin_cache)
+    
+    return dA_prev, dW, db
+    
+
+def L_forward(X, parameters):
+    
+    caches = []
+    n_layers = len(parameters)//2
+    
+    A_prev = X
+    
+    for layer in range(1, n_layers):
+        W = parameters['W' + str(layer)]
+        b = parameters['b' + str(layer)]
+        Z, lin_cache = linear_forward(A_prev, W, b)
+        A, activ_cache = relu(Z)
+        
+        A_prev = A
+        
+        caches.append((lin_cache, activ_cache))
+    
+    WL = parameters['W' + str(n_layers)]
+    bL = parameters['b' + str(n_layers)]
+    ZL, lin_cache = linear_forward(A_prev, WL, bL)
+    AL, activ_cache = softmax(ZL) # 
+    
+    caches.append((lin_cache, activ_cache))
+    
+    return AL, caches
+
+
+def L_backward(AL, Y, caches):
+    
+    grads = {}
+    L = len(caches) # number of layers
+    m = Y.shape[1] # number of samples
+    
+    # compute the output vector from the model:
+    #S = softmax(AL)
+    #assert(S.shape == Y.shape)
+    
+    # initialize back propagation:
+    dAL = (-1/m) * np.divide(Y, AL)  # * S * (1 - S)
+        
+    current_cache = caches[L-1]
+    dA_prev, dW, db = activation_backward(dAL, current_cache, activation='softmax')
+    grads['dW' + str(L)] = dW
+    grads['db' + str(L)] = db
+    
+    for i in reversed(range(L-1)):
+        current_cache = caches[i]
+        dA_prev, dW, db = activation_backward(dA_prev, current_cache, activation='relu')
+        grads['dW' + str(i+1)] = dW
+        grads['db' + str(i+1)] = db
+    
+    return grads
+
---
+++ ann_utils.py
@@ -0,0 +1,163 @@
+'''
+Helper Functions in utils:
+
+relu(Z):
+	A, Z
+
+sigmoid(Z):
+	A, Z
+
+softmax(Z):
+	A, Z
+
+relu_backward(dA):
+	dZ
+
+sigmoid_backward(dA):
+	dZ
+
+softmax_backward(dA):
+	dZ
+
+cost_total(AL, Y):
+	cost
+'''
+
+import numpy as np
+
+def relu(Z):
+    '''
+    Computes elementwise ReLU of the input, where
+    relu(x) = max(0, x)
+
+    Input:
+    Z = np.array of shape (layer_size, n_samples)
+
+    Output:
+    A = np.array with elementwise ReLU's of Z
+    cache = Z, stored for later use in backprop
+    '''
+
+    A = np.maximum(0, Z)
+    cache = Z
+    
+    return A, cache
+
+def sigmoid(Z):
+    '''
+    Computes elementwise Sigmoid of the imput
+
+    Input:
+    Z = np.array of shape (layer_size, n_samples)
+
+    Output:
+    A = np.array with elementwise sigmoids of Z
+    cache = Z, stored for later use in backprop
+    '''
+
+    A = 1 / (1 + np.exp(-Z))
+    cache = Z
+    assert(A.shape == Z.shape)
+
+    return A, cache
+
+def softmax(Z):
+    '''
+    Computes softmax of Z
+    
+    Inputs:
+    Z = np.array of shape (layer_size, n_samples)
+    
+    Outputs:
+    y = np.array of shape (n_classes, n_samples)
+    '''
+    
+    z_exp = np.exp(Z)
+    sum_z_exp = np.sum(z_exp, axis=0, keepdims=True)
+
+    assert(sum_z_exp.shape == (1, Z.shape[1]))
+    
+    A = z_exp / sum_z_exp
+
+    cache = Z
+    assert(A.shape == Z.shape)
+    
+    return A, cache
+
+def relu_backward(dA, cache):
+    '''
+    Computes the deriative of ReLU.
+
+    Inputs:
+    dA = np.array with the derivative of the cost w/respect to the output A of a layer
+    cache = np.array Z from forward prop
+
+    dZ = np.array with the derivative of the cost w/respect to Z of the layer
+    '''
+
+    Z = cache
+
+    assert(dA.shape == Z.shape)
+    
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0
+    
+    return dZ
+
+def sigmoid_backward(dA, cache):
+    '''
+    Computes the deriative of sigmoid.
+
+    Inputs:
+    dA = np.array with the derivative of the cost w/respect to the output A of a layer
+    cache = np.array Z from forward prop
+
+    dZ = np.array with the derivative of the cost w/respect to Z of the layer
+    '''
+    
+    Z = cache
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    
+    return dZ
+
+def softmax_backward(dA, cache):
+    '''
+    Computes the deriative of softmax.
+
+    Inputs:
+    dA = np.array with the derivative of the cost w/respect to the output A of a layer
+    cache = np.array Z from forward prop
+
+    Outputs:
+    dZ = np.array with the derivative of the cost w/respect to Z of the layer
+    '''
+    
+    Z = cache
+    
+    A, cache = softmax(Z)
+    
+    dZ = dA * A * (1 - A)
+    
+    return dZ
+
+def cost_total(AL, Y):
+    '''
+    Computes the cost function associated with a set of predictions AL and ground truth labels Y.
+
+    Inputs:
+    AL = np.array with predictions, AL.shape = (output_size, n_samples)
+    Y = np.array with ground truth labels, Y.shape = (output_size, n_samples)
+
+    Outputs:
+    cost = float, logLoss cost associated with the predictions and for the correct labels. 
+    '''
+    
+    assert(AL.shape == Y.shape)
+    
+    m = AL.shape[1]
+    
+    loss = np.log(np.sum(AL*Y, axis=0, keepdims=True))
+    cost = (-1 / m) * np.sum(loss)
+    
+    return cost
